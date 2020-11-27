26fb30406a86915a38a976108268f6965f1618b6
YUGPATEL20 <70703985+YUGPATEL20@users.noreply.github.com>
2020-11-27 19:12:16 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var car,wall;
+var speed,height;
+
+
+function setup(){   
+    createCanvas(1600,400);
+
+speed=random(55,90);
+weight=random(400,1500);
+
+    car=createSprite(50,200,50,50);
+
+
+    wall=createSprite(1300,200,60,height/2)
+    wall.shapeColour=color(80,80,80);
+}
+
+function draw() {
+    background("black");
+    car.velocityX = speed;
+    deformation();
+    drawSprites();
+}
+
+function deformation(){
+    if(wall.x - car.x<(car.width+ wall.width)/2){
+        car.velocityX = 0;
+    
+        deform = (0.5*weight*speed*speed)/22500;
+        console.log(deform);
+     
+        if(deform > 180){
+            car.shapeColor= color(225,0,0);
+        }
+        if(deform< 180 && deform >  100){
+            car.shapeColor=color(230,230,0);
+        }
+        if(deform < 100){
+            car.shapeColor = color(0,255,0);
+
+        }
+    }
+}
